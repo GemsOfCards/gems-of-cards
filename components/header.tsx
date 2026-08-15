8eb2b370446cdfcfe53127180f5e82c6c4da49e3
GemsOfCards <prathamshenoy2003@gmail.com>
2026-08-15 13:41:57 +0530

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,1 +1,16 @@
-import Link from "next/link"; export default function Header(){return <header><Link href="/" className="brand"><span className="mark">G</span><span>GEMS OF CARDS</span></Link><nav><Link href="/">HOME</Link><Link href="/collection">COLLECTIONS</Link><Link href="/featured">FEATURED GEMS</Link><Link className="goldlink" href="/admin">COLLECTION MANAGER</Link></nav></header>}
+import Link from "next/link";
+
+export default function Header(){
+  return <header>
+    <Link href="/" className="brand" aria-label="Gems of Cards home">
+      <img className="brandLogo" src="/gems-of-cards-logo.png" alt="Gems of Cards logo" />
+      <span className="brandWords"><strong>GEMS</strong><small>OF</small><strong>CARDS</strong></span>
+    </Link>
+    <nav>
+      <Link href="/">HOME</Link>
+      <Link href="/collection">COLLECTIONS</Link>
+      <Link href="/featured">FEATURED GEMS</Link>
+      <Link className="goldlink" href="/admin">COLLECTION MANAGER</Link>
+    </nav>
+  </header>
+}
